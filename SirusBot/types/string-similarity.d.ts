be2612a97867c092e8115956ccda815dafb0a85e
alexsander2002 <alex13filipi@gmail.com>
2025-10-18 21:18:30 -0300

    feat: organizar branch SirusBot com arquivos do projeto SirusBot na raiz

--- SirusBot/types/string-similarity.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "string-similarity" {
-  const compare: any
-  export default compare
-} 
--- SirusPay/types/nprogress.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "nprogress" {
-  const NProgress: {
-    configure: (options: Record<string, unknown>) => void
-    start: () => void
-    done: () => void
-  }
-  export default NProgress
-} 
